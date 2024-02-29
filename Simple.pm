8e79f25bc0b11de8e7c1afc61c4cbd12981e47d3
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-29 16:29:48 +0100

    Add documentation

--- Simple.pm
+++ Simple.pm
@@ -36,3 +36,138 @@ sub BUILD {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Data::Text::Simple - Data object for text in language.
+
+=head1 SYNOPSIS
+
+ use Data::Text::Simple;
+
+ my $obj = Data::Text::Simple->new(%params);
+ my $id = $obj->id;
+ my $lang = $obj->lang;
+ my $text = $obj->text;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Data::Text::Simple->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<id>
+
+Id of record.
+Id could be number.
+
+It's optional.
+
+Default value is undef.
+
+=item * C<lang>
+
+Language ISO 639-1 code.
+
+It's optional.
+
+=item * C<text>
+
+Main text.
+
+It's optional.
+
+=back
+
+Returns instance of object.
+
+=head2 C<id>
+
+ my $id = $obj->id;
+
+Get id.
+
+Returns number.
+
+=head2 C<lang>
+
+ my $lang = $obj->lang;
+
+Get language ISO 639-1 code.
+
+Returns string.
+
+=head2 C<text>
+
+ my $text = $obj->text;
+
+Get text.
+
+Returns string.
+
+=head1 ERRORS
+
+ new():
+         Parameter 'id' must be a number.
+                 Value: %s
+
+=head1 EXAMPLE
+
+=for comment filename=create_and_print_text.pl
+
+ use strict;
+ use warnings;
+
+ use Data::Text::Simple;
+
+ my $obj = Data::Text::Simple->new(
+         'id' => 7,
+         'lang' => 'en',
+         'text' => 'This is a text.',
+ );
+
+ # Print out.
+ print 'Id: '.$obj->id."\n";
+ print 'Language: '.$obj->lang."\n";
+ print 'Text: '.$obj->text."\n";
+
+ # Output:
+ # Id: 7
+ # Language: en
+ # Text: This is a text.
+
+=head1 DEPENDENCIES
+
+L<Mo>,
+L<Mo::utils>,
+L<Mo::utils::Language>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Data-Text-Simple>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2023-2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
--- xt/Data-Text-Simple/01-pod_coverage.t
+++ xt/Data-Text-Simple/01-pod_coverage.t
@@ -5,4 +5,6 @@ use Test::NoWarnings;
 use Test::Pod::Coverage 'tests' => 2;
 
 # Test.
-pod_coverage_ok('Data::Text::Simple', 'Data::Text::Simple is covered.');
+pod_coverage_ok('Data::Text::Simple',
+	{ 'also_private' => ['BUILD'] },
+	'Data::Text::Simple is covered.');
